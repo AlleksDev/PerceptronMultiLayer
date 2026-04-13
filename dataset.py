8d064ffca39a2bf8efdc07b08453fc9871891b54
AlleksDev <aimr2910@gmail.com>
2026-04-13 01:33:58 -0600

    First version

---
+++ dataset.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from d2l import tensorflow as d2l
+
+
+class KaggleHouse(d2l.DataModule):
+    def __init__(self, batch_size, train=None, val=None):
+        super().__init__()
+        self.save_hyperparameters()
+        if self.train is None:
+            self.raw_train = pd.read_csv(d2l.download(
+                d2l.DATA_URL + 'kaggle_house_pred_train.csv', self.root,
+                sha1_hash='585e9cc93e70b39160e7921475f9bcd7d31219ce'))
+            self.raw_val = pd.read_csv(d2l.download(
+                d2l.DATA_URL + 'kaggle_house_pred_test.csv', self.root,
+                sha1_hash='fa19780a7b011d9b009e8bff8e99922a8ee2eb90'))
